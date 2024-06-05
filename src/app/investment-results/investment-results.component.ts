bbfea729ee211f0afe6931d3d317283739b82762
zeinab <104681580+ZeinabIb@users.noreply.github.com>
2024-06-05 13:10:36 +0300

    investment service

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 @Component({
   selector: 'app-investment-results',
   standalone: true,
@@ -8,13 +9,12 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  @Input({required:true}) results?: {
-    year:number;
-    interest:number;
-    valueEndOfYear:number;
-    annualInvestment:number;
-    totalInterest:number;
-    totalAmountInvested:number;
-  }[];
+  
+  constructor(private investmentService :InvestmentService){}
+
+  get results(){
+    return this.investmentService.resultsData;
+  }
+
 
 }
